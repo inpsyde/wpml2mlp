b4276d6219760afff7dbd9f8ef2de88a21b3ae46
Davor Santic <dsantic@autoscout24.com>
2014-10-06 22:37:42 +0200

    Initial test commit
    Intial test commit with plugin definition

---
+++ wpml2mlp.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WPML 2 MLP
+ * Plugin URI:  http://marketpress.com/product/multilingual-press-pro/?piwik_campaign=mlp&piwik_kwd=pro
+ * Description: Get data from WPML export and immediately import in Multisite environment .
+ * Author:      Inpsyde GmbH
+ * Author URI:  http://inpsyde.com
+ * Version:     1.0.0
+ * Network:     true
+ */
+
