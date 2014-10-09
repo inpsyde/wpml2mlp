396e4860fcc368e2f6e4bb1a31d1a2efc341e7ce
Davor Santic <dsantic@autoscout24.com>
2014-10-10 01:31:56 +0200

    wpml2mlp - creating new site logic
    Use  filter “wpmu_new_blog” for setting the correct site_options;
    relations…

--- inc/mlp-site-creator.php
+++ inc/mlp-site-creator.php
@@ -46,17 +46,17 @@ class MLP_Site_Creator {
 	 */
 	public function create_site( $language ) {
 
-		$is_multisite_on_subdomain = self::check_is_subdomain_multisite_running();
+		$is_multisite_on_subdomain = $this->check_is_subdomain_multisite_running();
 		$current_site              = get_current_site();
 		$domain                    = $is_multisite_on_subdomain ? $lng . $current_site->domain : $current_site->domain;
 		$path                      = $is_multisite_on_subdomain ? "/" : "/" . $lng;
 		$user_id                   = get_current_user_id();
 
-		$new_blog_id = wpmu_create_blog( $domain, $path, strtoupper( $language ) . " site", $user_id );
+		//set this before creating the new blog
+		//set correct $_POST params, how we can use wpmu_new_blog filter for writing the correct data
+		$this->set_or_update_post_obj( $this->convert_to_lang_obj( $language ) );
 
-		$site_meta_arr = maybe_unserialize( get_site_option( "inpsyde_multilingual" ) );
-
-		WPML2MLP_Helper::append_and_update_site_meta( $site_meta_arr, $language, $new_blog_id );
+		wpmu_create_blog( $domain, $path, strtoupper( $language ) . " site", $user_id );
 
 		$this->site_exists_arr[ $language ] = TRUE;
 	}
@@ -64,11 +64,19 @@ class MLP_Site_Creator {
 	/**
 	 * Checks how the multisite is configured (subdomain or folder separation)
 	 *
-	 * @return type
+	 * @return bool
 	 */
 	private function check_is_subdomain_multisite_running() {
 
 		return defined( 'SUBDOMAIN_INSTALL' ) ? SUBDOMAIN_INSTALL : FALSE;
+	}
+
+	private function set_or_update_post_obj( $convert_to_lang_obj ) {
+		//TODO implement writing to $_POST object
+	}
+
+	private function convert_to_lang_obj( $language ) {
+		//TODO construct to correct language object. Probbably it will be call to $this->language_api->get_db()->get_items( $search, OBJECT );
 	}
 
 }
