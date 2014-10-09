2cb80feb6a537f739bd5ebe23c4345594e773836
Davor Santic <dsantic@autoscout24.com>
2014-10-10 01:33:54 +0200

    wpml2mlp - remove method
    Remove unused methods

--- inc/wpml2mlp-helper.php
+++ inc/wpml2mlp-helper.php
@@ -13,17 +13,4 @@ class WPML2MLP_Helper {
 
 		return $ret;
 	}
-
-	public static function append_and_update_site_meta( &$site_meta_arr, $lng, $new_blog_id, $text = "" ) {
-
-		$new_meta =
-			array(
-				'lang' => $lng,
-				'text' => $text,
-			);
-
-		$site_meta_arr[ $new_blog_id ] = $new_meta;
-
-		update_site_option( "inpsyde_multilingual", serialize( $site_meta_arr ) );
-	}
 }
