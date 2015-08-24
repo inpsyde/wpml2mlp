ca1ef114cd87f679e859e0e443bab23a175ae531
Frank Bültge <frank@bueltge.de>
2015-08-24 15:12:07 +0200

    refactor for codex, spelling

--- inc/Wpml2mlp_Categorie_Creator.php
+++ inc/Wpml2mlp_Categorie_Creator.php
@@ -1,7 +1,8 @@
 <?php
-
 /**
  * Class Wpml2mlp_Categorie_Creator
+ *
+ * @version 2015-08-24
  */
 class Wpml2mlp_Categorie_Creator {
 
@@ -20,13 +21,15 @@ class Wpml2mlp_Categorie_Creator {
 	/**
 	 * Constructs the WPML2MLP_Categorie_Creator
 	 *
+	 * @param wpdb                            $wpdb
+	 * @param Mlp_Content_Relations_Interface $content_relations
 	 */
 	public function __construct(
 		wpdb $wpdb,
 		Mlp_Content_Relations_Interface $content_relations
 	) {
 
-		if ( NULL == $wpdb ) {
+		if ( NULL === $wpdb ) {
 			return;
 		}
 
@@ -38,55 +41,52 @@ class Wpml2mlp_Categorie_Creator {
 	 * Creates the categories from the language
 	 *
 	 * @param $blog
-	 *
 	 */
 	public function create_categories_from_lng( $blog ) {
 
 		global $sitepress;
 
 		$current_language = $sitepress->get_current_language();
-		$blog_language = get_blog_language( $blog[ 'blog_id' ], TRUE );
+		$blog_language    = get_blog_language( $blog[ 'blog_id' ], TRUE );
 
-		$sitepress->switch_lang($blog_language);
+		$sitepress->switch_lang( $blog_language );
 
 		$get_cat_args = array(
-			"hide_empty" => 0
+			'hide_empty' => 0
 		);
-		$categories = get_categories($get_cat_args);
+		$categories   = get_categories( $get_cat_args );
 
-		$sitepress->switch_lang($current_language);
+		$sitepress->switch_lang( $current_language );
 
 		switch_to_blog( (int) $blog[ 'blog_id' ] );
 
-		foreach ( $categories as $current_categorie ) {
+		foreach ( $categories as $current_category ) {
 
-			if(!term_exists($current_categorie->name, 'category')){
-				$mlp_categorie_id = self::add_categorie( $current_categorie );
+			if ( ! term_exists( $current_category->name, 'category' ) ) {
+				$mlp_category_id = self::add_category( $current_category );
 			}
 		}
 
 		restore_current_blog();
-
 	}
 
 	/**
-	 * Adds the categorie to the relevant site
+	 * Adds the category to the relevant site
 	 *
-	 * @param $categorie
+	 * @param array $category
 	 *
 	 * @return int
 	 */
-	public function add_categorie( $categorie ) {
+	public function add_category( $category ) {
 
-		$categorie_args = array(
-			'cat_name' => $categorie->name,
-			'category_description' => $categorie->description,
-			'category_nicename' => $categorie->slug,
-			'category_parent' => $categorie->parent
+		$category_args = array(
+			'cat_name'             => $category->name,
+			'category_description' => $category->description,
+			'category_nicename'    => $category->slug,
+			'category_parent'      => $category->parent
 		);
 
-		return wp_insert_category($categorie_args);
-
+		return wp_insert_category( $category_args );
 	}
 
 }
