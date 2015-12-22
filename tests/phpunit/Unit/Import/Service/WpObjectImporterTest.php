c0ee12783b11e2073b5c40f00dbaf7cd8e5dca5d
David Naber <kontakt@dnaber.de>
2015-12-22 16:50:40 +0100

    Mock WP_Post to avoid errors with type hinting classes

--- tests/phpunit/Unit/Import/Service/WpObjectImporterTest.php
+++ tests/phpunit/Unit/Import/Service/WpObjectImporterTest.php
@@ -16,11 +16,16 @@ class WpObjectImporterTest extends \PHPUnit_Framework_TestCase {
 	protected function setUp(){
 
 		if( ! $this->fs_helper ){
-
 			$this->fs_helper = new Helper\FileSystem;
-
 		}
 
+		/**
+		 * Just create some mocks of these types to avoid
+		 * error messages like this
+		 * https://github.com/sebastianbergmann/phpunit-mock-objects/issues/273
+		 * when mocking objects that type hint WP core components
+		 */
+		$this->getMock( 'WP_Post' );
 	}
 
 	/**
@@ -35,8 +40,10 @@ class WpObjectImporterTest extends \PHPUnit_Framework_TestCase {
 	 */
 	public function test_import_term(){
 
-		$translation_connector_mock = $this->getMockBuilder( 'W2M\Import\Service\TranslationConnectorInterface' )->getMock();
-		$id_mapper_mock = $this->getMockBuilder( 'W2M\Import\Data\IdMapperInterface' )->getMock();
+		$translation_connector_mock = $this->getMockBuilder( 'W2M\Import\Service\TranslationConnectorInterface' )
+			->getMock();
+		$id_mapper_mock = $this->getMockBuilder( 'W2M\Import\Data\IdMapperInterface' )
+			->getMock();
 
 		$testee = new Service\WpObjectImporter( $translation_connector_mock, $id_mapper_mock );
 
