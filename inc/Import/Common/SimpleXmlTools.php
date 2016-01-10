8e87439c21b4177403381c6fd401a7b97eed4197
David Naber <kontakt@dnaber.de>
2016-01-10 16:55:01 +0100

    Introduce Common\SimpleXmlTools

---
+++ inc/Import/Common/SimpleXmlTools.php
@@ -0,0 +1,24 @@
+<?php # -*- coding: utf-8 -*-
+
+namespace W2M\Import\Common;
+
+use
+	SimpleXMLElement;
+
+class SimpleXmlTools {
+
+	/**
+	 * @param SimpleXMLElement $document
+	 * @param $namespace
+	 *
+	 * @return string|null
+	 */
+	public function get_doc_namespace( SimpleXMLElement $document, $namespace ) {
+
+		$doc_ns = $document->getDocNamespaces();
+
+		return isset( $doc_ns[ $namespace ] )
+			? $doc_ns[ $namespace ]
+			: NULL;
+	}
+}
