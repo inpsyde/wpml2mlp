43137890e2787bd2b59773fdb1d7a3d74462928a
David Naber <kontakt@dnaber.de>
2016-02-07 15:43:00 +0100

    Introduce *PassThroughFilter #49

---
+++ inc/Import/Filter/CommentPassThroughFilter.php
@@ -0,0 +1,22 @@
+<?php # -*- coding: utf-8 -*-
+
+namespace W2M\Import\Filter;
+
+use
+	W2M\Import\Type;
+
+class CommentPassThroughFilter implements CommentImportFilterInterface {
+
+	/**
+	 * Checks if a comment should be imported or not
+	 *
+	 * @param Type\ImportCommentInterface $import_comment
+	 *
+	 * @return bool
+	 */
+	public function comment_to_import( Type\ImportCommentInterface $import_comment ) {
+
+		return TRUE;
+	}
+
+}
---
+++ inc/Import/Filter/TermPassThroughFilter.php
@@ -0,0 +1,22 @@
+<?php # -*- coding: utf-8 -*-
+
+namespace W2M\Import\Filter;
+
+use
+	W2M\Import\Type;
+
+class TermPassThroughFilter implements TermImportFilterInterface {
+
+	/**
+	 * Checks if a term should be imported or not
+	 *
+	 * @param Type\ImportTermInterface $import_term
+	 *
+	 * @return bool
+	 */
+	public function term_to_import( Type\ImportTermInterface $import_term ) {
+
+		TRUE;
+	}
+
+}
